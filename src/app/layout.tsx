05b2043b3532460d177876de23d8bf6555ab17cb
kev-o <97689028+kevo-dev@users.noreply.github.com>
2024-04-18 15:53:50 +0000

    modified:   package.json 	modified:   pnpm-lock.yaml 	modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,15 @@
 import { PropsWithChildren } from 'react';
 import 'devicon';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({ children }: PropsWithChildren) {
-  return children;
+  return (
+    <>
+      <Analytics /> 
+      <SpeedInsights/>
+      {children}
+    </>
+  );
 }
